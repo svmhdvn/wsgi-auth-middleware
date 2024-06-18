f884c617da0fcb26f8d315bfb472d997ef422c8c
Siva Mahadevan <me@svmhdvn.name>
2024-06-18 12:38:18 -0400

    s/pam.PamAuthenticator()/pam.pam()

--- src/wsgi_auth_middleware/backends/pam.py
+++ src/wsgi_auth_middleware/backends/pam.py
@@ -4,7 +4,7 @@ import pam
 class PamBackend:
     def __init__(self, service="login"):
         self.service = service
-        self.pam = pam.PamAuthenticator()
+        self.pam = pam.pam()
 
     def __call__(self, username, password):
         return pam.authenticate(username, password, service=self.service)
